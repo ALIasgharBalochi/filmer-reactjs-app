1f51011463e513b1969dfa677c571a5cad77ddfc
ali balochi <alibalochi191077@gmail.com>
2023-10-21 17:25:39 +0330

    change skeleton count

--- src/components/SkeletonLoading.jsx
+++ src/components/SkeletonLoading.jsx
@@ -4,7 +4,7 @@ import Skeleton from 'react-loading-skeleton'
 
 const SkeletonLoading = () => {
 return(
-         <Skeleton inline={true} count={10} style={{margin: '0 .5rem'}} width={100} height={200}/> 
+         <Skeleton inline={true} count={20} style={{margin: '0 .5rem'}} width={100} height={200}/> 
     )
 }
 export default SkeletonLoading;
